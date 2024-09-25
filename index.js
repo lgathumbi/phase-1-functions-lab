ca68e5aaf3637b7a6d480e3ff4abe763d8de5cab
lorna gathumbi <nyawiralornah@gmail.com>
2024-09-25 13:46:52 +0300

    js functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks *264;
+}
+function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(destination - start);
+    return distanceInBlocks *264;
+}
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    
+    if (distanceInFeet <=400) {
+        return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <=2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
